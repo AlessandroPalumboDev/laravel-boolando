8b68ee26de4a2abcb5b4663a86f260c5ec1b5916
Alessandro Palumbo <163033493+AlessandroPalumboDev@users.noreply.github.com>
2024-07-05 21:54:00 +0200

    [UPD] Aggiunto bottone ad home

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,4 +3,7 @@
 @section('main')
 <div class="col text-center">
     <h1>{{$title}}</h1>
+    <div>
+        <button type="button" class="btn btn-warning"><a class="selezione" href="{{route('products')}}">Guarda i nostri Prodotti</a></button>
+    </div>
 </div>@endsection
